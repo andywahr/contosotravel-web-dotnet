1939b3e8ad6918c340083d9db62e5c9bdbf046bd
Andy Wahrenberger <andywahr@microsoft.com>
2019-03-09 23:45:23 -0600

    fun with nuget

---
+++ src/Host.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContosoTravel.Web.Host.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
